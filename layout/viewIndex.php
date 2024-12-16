acc345b1ded41da37498d0c33e67e5639f668d0f
Gabriel Ruiz Estrella <estrella120801@gmail.com>
2024-12-16 13:30:22 -0600

    Agregando archivos

---
+++ layout/viewIndex.php
@@ -0,0 +1,6 @@
+<?php
+    function llamandoRecursos(){
+        echo '<link rel="stylesheet" href="public/css/paleta.css">';
+        echo '<link rel="stylesheet" href="public/css/index.css">';
+    }
+?>
